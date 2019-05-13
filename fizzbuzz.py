0305cddfb150ce3c2c31e25b1feb78453b947d4c
cabos <yuto.eguma@gmail.com>
2019-05-13 21:59:34 +0900

    add document

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,6 +3,19 @@
 
 
 def fizzbuzz(value):
+    """Simple process of fizzbuzz.
+    This Documents follow by numpydoc(https://numpydoc.readthedocs.io/en/latest/format.html)
+
+    Parameters
+    ----------
+    value: int
+        Int value, this value will converted by fizzbuzz process
+
+    Returns
+    ----------
+    fizzbuzz: str
+    """
+
     if value % 15 == 0:
         return "fizzbuzz"
     elif value % 3 == 0:
